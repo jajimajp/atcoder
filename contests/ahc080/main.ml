ce7b79e7e9d3211eeea23346361ae362a8753a6f
Yajima Soichi <jajima.jp@gmail.com>
2024-02-09 21:27:23 +0900

    油田があった場所の近くを探索するように並べ替え

--- contests/ahc080/main.ml
+++ contests/ahc080/main.ml
@@ -35,6 +35,34 @@ let input () : t =
 let dist_p2 (i1, j1) (i2, j2) =
   (i1-i2)*(i1-i2) + (j1-j2)*(j1-j2)
 
+(** ２次元配列 arr の (i, j) 周囲８マスの値を value だけ増加 *)
+let set_chance_around t arr i j value =
+  for di = -1 to 1 do
+    let i' = i + di in
+    if i' < 0 || t.n <= i' then ()
+    else
+      for dj = -1 to 1 do
+        if di = 0 && dj = 0 then ()
+        else
+          let j' = j + dj in
+          if j' < 0 || t.n <= j' then ()
+          else
+            arr.(i').(j') <- arr.(i').(j') + value
+      done
+  done
+
+(** 期待の２次元配列 arr から、２点の比較関数を返す *)
+let compare_points t arr p1 p2 =
+  let i1, j1 = p1 in
+  let i2, j2 = p2 in
+  if arr.(i1).(j1) > arr.(i2).(j2) then
+    -1
+  else if arr.(i1).(j1) < arr.(i2).(j2) then
+    1
+  else
+    let center = (t.n / 2, t.n / 2) in
+    (dist_p2 p1 center) - (dist_p2 p2 center)
+
 exception Break
 let () =
   let t = input () in
@@ -46,27 +74,31 @@ let () =
     List.Cartesian_product.map2
       (List.range 0 t.n) (List.range 0 t.n)
       ~f:(fun a b -> (a, b)) in
+  (* マスの期待値を表す *)
+  let chance = Array.make_matrix ~dimx:t.n ~dimy:t.n 0 in
   (* 座標を中心に近い順に並べ替える *)
   let center = (t.n / 2, t.n / 2) in
   let check_poss = List.sort check_poss
       ~compare:(fun p1 p2 ->
         (dist_p2 p1 center) - (dist_p2 p2 center)) in
-  try
-    List.iter check_poss
-      ~f:(fun (i, j) ->
-        printf "q 1 %d %d\n" i j;
-        Out_channel.flush Out_channel.stdout;
-        let inp = read_int () in
-        match inp with
-        | 0 -> ()
-        | n ->
+  let rec loop = function
+    | [] -> ()
+    | (i, j) :: tl ->
+      printf "q 1 %d %d\n" i j;
+      Out_channel.flush Out_channel.stdout;
+      let inp = read_int () in
+      begin match inp with
+      | 0 -> ()
+      | n ->
           v_found := !v_found + n;
-          pos := (i, j) :: !pos;
-        if !v_found = v_tot then
-          raise Break
-      )
-  with
-  | Break -> ();
+          set_chance_around t chance i j n;
+          pos := (i, j) :: !pos
+      end;
+      if !v_found = v_tot then () (* 探索完了 *)
+      else
+        let compare = compare_points t chance in
+        loop (List.sort tl ~compare) in
+  let () = loop check_poss in
   let ijstr =
     List.map !pos ~f:(fun (i, j) -> Printf.sprintf "%d %d" i j)
     |> String.concat ~sep:" " in
